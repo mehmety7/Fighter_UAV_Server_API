c8bb7e416a1bf091bf74cfd10792f4291930a453
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2022-01-14 12:57:21 +0300

    add bean annotation

--- src/main/java/com/bam/fuavserver/repo/LoginRepository.java
+++ src/main/java/com/bam/fuavserver/repo/LoginRepository.java
@@ -4,15 +4,11 @@ import com.bam.fuavserver.model.entity.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoginRepository extends JpaRepository<Login, Long> {
 
     Login getLoginByKadi(String kadi);
-
-    @Query(nativeQuery = true, value = "UPDATE LOGIN SET IN_OUT=FALSE")
-    void setFalseAllLogin();
-
-    @Query(nativeQuery = true, value = "UPDATE LOGIN l SET IN_OUT=TRUE WHERE l.id = :id")
-    void setTrueLoginById(@Param("id") Long id);
 
 }
--- src/main/java/com/bam/fuavserver/repo/TelemetrySenderRepository.java
+++ src/main/java/com/bam/fuavserver/repo/TelemetrySenderRepository.java
@@ -4,7 +4,9 @@ import com.bam.fuavserver.model.entity.TelemetrySender;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TelemetrySenderRepository extends JpaRepository<TelemetrySender, Long> {
 
     @Query(value = "SELECT t.id FROM TELEMETRY_SENDER t WHERE t.takim_numarasi=:number",nativeQuery = true)
